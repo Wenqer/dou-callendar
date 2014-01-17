db9501ee8a7d76af8ec07cd46e0b3e1f7060e36e
vagrant <vagrant@precise64.(none)>
2014-01-17 16:32:56 +0000

    test

---
+++ app/lib/dou.js
@@ -0,0 +1,58 @@
+
+exports.data = function() {
+
+//    var req = new XMLHttpRequest();
+//    req.open('GET', 'http://dou.ua/calendar/', false);
+//    req.send(null);
+//
+//    var links = parseHTML(req.responseText).getElementsByClassName('events');
+//    var rows = [];
+//
+//    for (var index = 0; index < links.length; ++index) {
+//        var event = {
+//            articleUrl: 'sdfsdf',
+//            image: "sdf",
+//            title: links[index].getElementsByClassName('place'),
+//            date: links[index].getElementsByClassName('date')
+//        }
+//        rows.push(event);
+//    }
+//
+//    console.log(rows);
+//    $.table.setData(rows);
+
+    var data = [
+        {
+            link: "",
+            image: "",
+            title: "new event",
+            date: "29748"
+        },{
+            link: "",
+            image: "",
+            title: "new event",
+            date: "29748"
+        },{
+            link: "",
+            image: "",
+            title: "new event",
+            date: "29748"
+        },{
+            link: "",
+            image: "",
+            title: "new event",
+            date: "29748"
+        },{
+            link: "",
+            image: "",
+            title: "new event",
+            date: "29748"
+        },{
+            link: "",
+            image: "",
+            title: "new event",
+            date: "29748"
+        }
+    ];
+    return data;
+};
